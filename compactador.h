68d37e329fb7c1617ccfde75dabeca29638b372b
Maria <cc20731@g.unicamp.br>
2021-11-22 12:23:16 -0300

    reorganização compactador

--- compactador.h
+++ compactador.h
@@ -10,13 +10,16 @@ class Compactador
   private:
     FILE* arqParaComp;
     FILE* arqComp;
+    char nomeArqParaComp[20];
     char nomeArqComp[20];
-    char nomeArqDesc[20];
     unsigned lBytes[256];
     unsigned int posicao;
     unsigned char c;
     unsigned int tamanho;
     unsigned char aux;
+ public:
+    Compactador();
+    void Compactar();
 };
 
 #endif
